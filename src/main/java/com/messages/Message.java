ef5bf3f9b2892bcaeab8ca93fc57cf0ce5b9c203
Theo Reyes <theo@Gizmo.(none)>
2012-09-02 15:58:59 +0800

    Added domain object - Message.java

---
+++ src/main/java/com/messages/Message.java
@@ -0,0 +1,42 @@
+package com.messages;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+    @Id @GeneratedValue
+    private int id;
+    private String sender;
+    private String text;
+    private Date timeStamp;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getSender() {
+        return sender;
+    }
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+}
